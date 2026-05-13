562adc62014b44e099a8ccdcdb656b2fb62a0705
ryuatnorcal <rmatsuda686ca@gmail.com>
2026-05-13 13:01:06 -0600

    updated font

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased bg-black">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
